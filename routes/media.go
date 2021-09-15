9a0e85fa331d9a36efe13de3270824ff424370bc
Nick Latham <nlatham@zagmail.gonzaga.edu>
2021-09-14 20:40:07 -0700

    feat: added basic routes for the media collection

--- routes/media.go
+++ routes/media.go
@@ -1,16 +1,24 @@
 package routes
 
 import (
-    "fmt"
-    // "os"
+	"context"
+	"fmt"
+	"net/http"
+	"time"
 
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/s3"
+
+	
+	"video-share/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson"
 	"github.com/gin-gonic/gin"
 )
 
-
+var mediaCollection *mongo.Collection = OpenCollection(Client, "media")
 
 func ListBuckets(c *gin.Context){
 	
@@ -34,3 +42,123 @@ func ListBuckets(c *gin.Context){
 	}
 }
 
+func AddMedia(c *gin.Context) {
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var media models.Media
+
+	if err := c.BindJSON(&media); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	validationErr := validate.Struct(media)
+	if validationErr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+		fmt.Println(validationErr)
+		return
+	}
+
+	media.ID = primitive.NewObjectID()
+
+	result, insertErr := mediaCollection.InsertOne(ctx, media)
+	if insertErr != nil {
+		msg := fmt.Sprintf("media object was not created")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+		fmt.Println(insertErr)
+		return
+	}
+	defer cancel()
+
+	c.JSON(http.StatusOK, result)
+}
+
+//get all media
+func GetAllMedia(c *gin.Context){
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	
+	var media []bson.M
+
+	cursor, err := mediaCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	
+	if err = cursor.All(ctx, &media); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+
+	fmt.Println(media)
+
+	c.JSON(http.StatusOK, media)
+}
+
+//get single media
+func GetSingleMedia(c *gin.Context){
+
+	mediaID := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(mediaID)
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var media bson.M
+
+	if err := mediaCollection.FindOne(ctx, bson.M{"_id": docID}).Decode(&media); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+
+	fmt.Println(media)
+
+	c.JSON(http.StatusOK, media)
+}
+
+//deletes all media
+func DeleteAllMedia(c * gin.Context){
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	result, err := mediaCollection.DeleteMany(ctx, bson.M{})
+	
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+
+	c.JSON(http.StatusOK, result.DeletedCount)
+}
+
+//deletes one media object
+func DeleteSingleMedia(c * gin.Context){
+	mediaID := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(mediaID)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	result, err := mediaCollection.DeleteOne(ctx, bson.M{"_id": docID})
+	
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+
+	c.JSON(http.StatusOK, result.DeletedCount)
+}
