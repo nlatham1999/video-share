4fdc0f3ce5e7299d469b743b85e42c7f3d7443bc
Nick Latham <nlatham@zagmail.gonzaga.edu>
2021-10-10 01:00:49 -0700

    Update main.go

--- main.go
+++ main.go
@@ -10,13 +10,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// simulate some private data
-var secrets = gin.H{
-	"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
-	"austin": gin.H{"email": "austin@example.com", "phone": "666"},
-	"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
-}
-
 func main() {
 
 	port := os.Getenv("PORT")
@@ -28,30 +21,15 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 
-	config := cors.DefaultConfig()
+	// config := cors.DefaultConfig()
 	// config.AllowHeaders = []string{"X-Auth-Token", "content-type"}
 	// config.ExposeHeaders = []string{"Content-Length"}
 	// config.AllowAllOrigins = true
-	config.AllowOrigins = []string{"https://www.videoshare.app"}
+	// config.AllowOrigins = []string{"https://www.videoshare.app"}
 
-	router.Use(cors.New(config))
-
-	// authorized := router.Group("/admin", gin.BasicAuth(gin.Accounts{
-	//     "foo":    "bar",
-	//     "austin": "1234",
-	//     "lena":   "hello2",
-	//     "manu":   "4321",
-	// }))
-
-	// authorized.GET("/secrets", func(c *gin.Context) {
-	//     // get user, it was set by the BasicAuth middleware
-	//     user := c.MustGet(gin.AuthUserKey).(string)
-	//     if secret, ok := secrets[user]; ok {
-	//         c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
-	//     } else {
-	//         c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
-	//     }
-	// })
+	// router.Use(cors.New(config))
+	
+	router.Use(cors.Default())
 
 	router.LoadHTMLGlob("index.html")
 
